028d031ff08494c7728ad05d5e561f7e1d642188
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-15 17:18:52 +0530

    Sign In & Sign Up pages.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,30 @@
+//render profile page
 module.exports.profile = function(req,res){
    return res.render('home',{
        title:'User Profile'
    });
+}
+
+//render sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+       title:'e-katta | Sign Up'
+    })
+}
+
+//render sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+       title:'e-katta | Sign In'
+    })
+}
+
+//get the sign up data
+module.exports.createUser = function(req,res){
+
+}
+
+//log in and create user session
+module.exports.createSession = function(req,res){
+    
 }
