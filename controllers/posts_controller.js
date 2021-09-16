8086de8dbad9251446c512d7df927c24ceb5e052
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-16 21:41:56 +0530

    added deleting posts functionality.

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,14 +1,33 @@
-const Post = require('../models/post')
+const Post = require("../models/post");
+const Comment = require("../models/comment");
 
-module.exports.createPost = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){
-            console.log('error in creating post');
-            return;
-        }
-        return res.redirect('back');
-    });
-}
+module.exports.createPost = function (req, res) {
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
+        console.log("error in creating post");
+        return;
+      }
+      return res.redirect("back");
+    }
+  );
+};
+
+module.exports.destroyPost = function (req, res) {
+  Post.findById(req.params.id, function (error, post) {
+    // .id means converting the object id into string
+    if (post.user == req.user.id) {
+      post.remove();
+
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        return res.redirect("back");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
