cf49d600276af64c1911686959f48cf2ef7d3801
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-16 17:48:06 +0530

    Displaying Posts.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,20 @@
-module.exports.home = function(req,res){
-    return res.render('home', {
-        title: 'Home'
-    });
-}
+const Post = require("../models/post");
+
+module.exports.home = function (req, res) {
+//   Post.find({}, function (err, posts) {
+//     return res.render("home", {
+//       title: "e-katta | Home",
+//       posts: posts
+//     });
+//   });
+
+//populate user of each  post
+  Post.find({}).populate('user').exec(function(err,posts){
+    return res.render("home", {
+        title: "e-katta | Home",
+        posts: posts
+      });
+  });
+
+
+};
