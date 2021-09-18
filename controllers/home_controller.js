7abbbdc9fe4527043f759aaa6a3bf8783c4cd780
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-18 19:42:21 +0530

    Ajax

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,28 @@
 const Post = require("../models/post");
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.home = async function (req, res) {
-
-  try{
+  try {
     //populate user of each  post
     let posts = await Post.find({})
+      .sort("-createdAt")
       .populate("user")
       .populate({
-          path: 'comments',
-          populate: {
-            path: 'user'
-          }
-      });
-  
-      let users = await User.find({});
-  
-      return res.render('home', {
-          title: "e-Katta | Home",
-          posts: posts,
-          all_users: users
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
 
-  } catch(err){
-      console.log('Error: ', err);
-      return;
+    let users = await User.find({});
+
+    return res.render("home", {
+      title: "e-Katta | Home",
+      posts: posts,
+      all_users: users,
+    });
+  } catch (err) {
+    console.log("Error: ", err);
+    return;
   }
-
-    
 };
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,21 +1,29 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
 
-module.exports.createPost = function (req, res) {
-  Post.create(
-    {
-      content: req.body.content,
-      user: req.user._id,
-    },
-    function (err, post) {
-      if (err) {
+module.exports.createPost = async function (req, res) {
+      try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
         req.flash('error', err);
-        return res.redirect("back");
-      }
-      req.flash('success', 'Post Published');
-      return res.redirect("back");
+        return res.redirect('back');
     }
-  );
 };
 
 module.exports.destroyPost = async function (req, res) {
@@ -28,7 +36,17 @@ module.exports.destroyPost = async function (req, res) {
           post.remove();
     
           await Comment.deleteMany({ post: req.params.id });
+         
+          if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post_id: req.params.id
+                },
+                message: "Post deleted"
+            });
+        }
           
+
           req.flash('success', 'Post is deleted');
 
           
