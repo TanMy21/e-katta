445207f013c2294596c52e54280b8b333d937ec9
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-19 09:41:25 +0530

    posts api

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,70 @@
+const Post = require('../../../models/post');
+
+const Comment = require('../../../models/comment');
+
+
+module.exports.index = async function(req,res){
+    
+    let posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+    
+    
+    return res.json(200,{
+        message: "List of Posts",
+        posts: posts
+    });
+}
+
+
+module.exports.destroyPost = async function (req, res) {
+
+    try{
+      let post = await Post.findById(req.params.id);
+     
+        // .id means converting the object id into string
+        // if (post.user == req.user.id) {
+          post.remove();
+    
+          await Comment.deleteMany({ post: req.params.id });
+         
+        //   if (req.xhr){
+        //     return res.status(200).json({
+        //         data: {
+        //             post_id: req.params.id
+        //         },
+        //         message: "Post deleted"
+        //     });
+        // }
+          
+
+        //   req.flash('success', 'Post is deleted');
+
+          
+        //   return res.redirect("back");
+        return res.json(200, {
+            message: "Posts and associated comments deleted."
+        });
+
+
+          
+        // } else {
+        //   req.flash('error', 'You Cannot delete this post');
+        //   return res.redirect("back");
+        // }
+    } catch(err) {
+        // req.flash('error', err);
+        // return res.redirect("back");
+        return res.json(500, {
+            message:"Internal Server Error"
+        });
+    }
+ 
+};
+
